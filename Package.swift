5c88a7f30c4d2a4bd727133dec8e6cd779eb99e8
JARMourato <joao.armourato@gmail.com>
2025-09-10 11:54:29 +0100

    update valet

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "StoragePlus", targets: ["StoragePlus"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Square/Valet", from: "4.0.0"),
+        .package(url: "https://github.com/Square/Valet", from: "5.0.0"),
     ],
     targets: [
         .target(name: "Storage", dependencies: [], path: "Sources/Core"),
